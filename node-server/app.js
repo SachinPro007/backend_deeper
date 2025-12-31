95096cf45f9871e3922949fb536ef63ca2dc3e3a
Sachin <mrsachinsehrawat07@gmail.com>
2025-12-31 17:26:54 +0530

    Learn deeper backend

---
+++ node-server/app.js
@@ -0,0 +1,12 @@
+const http = require("http")
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+  
+})
