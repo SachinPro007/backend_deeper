e9b5e66fdf831753833efa80e2c0e79d32860a7e
Sachin <mrsachinsehrawat07@gmail.com>
2026-01-02 20:32:56 +0530

    event loop, async code

--- node-server/practise/practise 2/resultHandler.js
+++ node-server/practise/practise 2/resultHandler.js
@@ -3,37 +3,42 @@ const { URLSearchParams } = require("url")
 
 const sumRequestHandler = (req, res) => {
   const body = []
+  let result;
   req.on("data", (chunk) => {
-    console.log(chunk);
+    console.log("log in chunk");
     body.push(chunk)
   })
 
   req.on("end", () => {
+    console.log("log in end");
+
     const fullBody = Buffer.concat(body).toString()
-    console.log(fullBody);
-
     const params = new URLSearchParams(fullBody)
-    console.log(params);
-
     const numbers = Object.fromEntries(params)
-    console.log(numbers);
 
-    const sum = parseInt(numbers.numberOne) + parseInt(numbers.numberTwo)
-    console.log(sum);
+    result = parseInt(numbers.numberOne) + parseInt(numbers.numberTwo)
 
-    res.write(`<html><body>
+    // Blocking code
+    // fs.writeFileSync("result.json", JSON.stringify({ result: result }))
+
+    // Async Opration
+    fs.writeFile("result.json", JSON.stringify({ result: result }), (err) => {
+      console.log("run after data writen");
+
+      res.write(`<html><body>
         <h1>Result</h1>      
-        <h1>${sum}</h1>      
+        <h1>${result}</h1>      
       </body></html>`)
 
-    fs.writeFileSync("result.json", JSON.stringify({ result: sum }))
+      return res.end()
 
-    return res.end()
+    })
+
   })
 
-
+  console.log("log outer");
 
 }
 
 
-module.exports = {sumRequestHandler}
+module.exports = { sumRequestHandler }
