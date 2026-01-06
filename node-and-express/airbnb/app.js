6a54b07e0421fcdf046e7b83dd49b1851c5d0b43
Sachin <mrsachinsehrawat07@gmail.com>
2026-01-06 17:52:51 +0530

    Serving static files, style server html file code

--- node-and-express/airbnb/app.js
+++ node-and-express/airbnb/app.js
@@ -18,6 +18,7 @@ const app = express()
 
 // app.use(parser.urlencoded())
 app.use(express.urlencoded())
+app.use(express.static(path.join(rootDir, "public")))
 
 app.use((req, res, next) => {
   console.log(req.url, req.method);
@@ -30,7 +31,6 @@ app.use("/host", hostRouter)
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(rootDir, "views", "404.html"))
 })
-
 
 
 
