7cfafe597ba91639cb0ab5c9a67307645cbe031e
Sachin <mrsachinsehrawat07@gmail.com>
2026-01-12 21:30:08 +0530

    perform curd opration in data file

--- node-and-express/dynamic-path/models/home.js
+++ node-and-express/dynamic-path/models/home.js
@@ -8,7 +8,8 @@ const fileDataPath = path.join(rootDir, "data", "homes.json")
 
 
 class Home {
-  constructor(houseName, price, location, rating, photoUrl) {
+  constructor(id, houseName, price, location, rating, photoUrl) {
+    this.id = id
     this.houseName = houseName;
     this.price = price;
     this.location = location;
@@ -17,16 +18,22 @@ class Home {
   }
 
   save() {
-    this.id = Math.random().toString();
-    Home.fetchAll((homes) => {      
-      homes.push(this)
-  
+    
+    Home.fetchAll((homes) => {  
+      if(this.id) { // update home
+        homes = homes.map(home => home.id === this.id ? this : home )
+
+      } else{ // add new home
+        this.id = Math.random().toString();        
+        homes.push(this)    
+      }  
+      
       fs.writeFile(fileDataPath, JSON.stringify(homes), (err) => {
         console.log(err);  
       })
-
     })
   }
+
 
   static fetchAll(callBack) {
     fs.readFile(fileDataPath, (err, data) => {
@@ -41,8 +48,13 @@ class Home {
       callBack(matchedHome)
     })
   }
-
   
+  static delete (homeId, callBack) {
+    Home.fetchAll((homes) => {
+      homes = homes.filter(home => home.id !== homeId)
+      fs.writeFile(fileDataPath, JSON.stringify(homes), callBack)
+    })
+  }  
 }
 
 module.exports = Home
--- node-and-express/dynamic-path/route/hostRoutes.js
+++ node-and-express/dynamic-path/route/hostRoutes.js
@@ -1,4 +1,4 @@
-const { getAddHome, postAddHome, getHostHomes } = require('../controllers/hostController')
+const { getAddHome, postAddHome, getHostHomes, getEditHome, postEditHome, postDeleteHome } = require('../controllers/hostController')
 
 const hostRouter = require('express').Router()
 
@@ -7,5 +7,10 @@ hostRouter.post("/add-home", postAddHome)
 
 hostRouter.get("/edit-home", postAddHome)
 hostRouter.get("/host-home-list", getHostHomes)
+
+hostRouter.get("/edit-home/:homeId", getEditHome)
+hostRouter.post("/edit-home", postEditHome)
+
+hostRouter.post("/delete-home/:homeId", postDeleteHome)
 
 module.exports = hostRouter
