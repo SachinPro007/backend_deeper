bac96e6cc135a804bede3c742bf5b38dc7bf2267
Sachin <mrsachinsehrawat07@gmail.com>
2026-01-24 21:09:39 +0530

    update and delete images

--- node-express-mongoose/fileUpload_download/controllers/hostController.js
+++ node-express-mongoose/fileUpload_download/controllers/hostController.js
@@ -1,11 +1,13 @@
+const fs = require("fs")
 const Home = require("../models/home")
+const rootDir = require("../utils/pathUtil")
 
 const getAddHome = (req, res, next) => {
-  res.render("host/edit-home", { 
-    pageTitle: "Add your Home on airbnb", 
-    editing: false, 
-    isLoggedIn: req.isLoggedIn, 
-    user: req.user 
+  res.render("host/edit-home", {
+    pageTitle: "Add your Home on airbnb",
+    editing: false,
+    isLoggedIn: req.isLoggedIn,
+    user: req.user
   })
 }
 
@@ -15,19 +17,19 @@ const getEditHome = async (req, res, next) => {
     const homeId = req.params.homeId
     const editing = req.query.editing === "true"
 
-    const home = await Home.findOne({_id: homeId})
+    const home = await Home.findOne({ _id: homeId })
 
     if (!home) {
       console.log("Home not found for editing");
       return res.redirect("/host/host-home-list")
     }
 
-    res.render("host/edit-home", { 
-      pageTitle: "Edit your home details", 
-      editing, 
-      home, 
-      isLoggedIn: req.isLoggedIn, 
-      user: req.user 
+    res.render("host/edit-home", {
+      pageTitle: "Edit your home details",
+      editing,
+      home,
+      isLoggedIn: req.isLoggedIn,
+      user: req.user
     })
 
   } catch (error) {
@@ -39,12 +41,23 @@ const getEditHome = async (req, res, next) => {
 const postEditHome = async (req, res, next) => {
   try {
     const { id, houseName, description, price, rating, location } = req.body
-    const updatedHome = { houseName, description, price, rating, location };
-    if(req.file){
-      updatedHome.photo = `/${req.file.path}`
-    }    
 
-    await Home.updateOne({_id: id}, updatedHome)
+    const home = await Home.findOne({ _id: id })
+
+    home.houseName = houseName
+    home.description = description
+    home.price = price
+    home.rating = rating
+    home.location = location
+
+    if (req.file) {      
+      fs.unlink(`${rootDir}/uploads/${home.photo}`, (err) => {
+        if (err) { console.log("Error while deleting file: ", err); }
+      })
+      home.photo = `${req.file.filename}`
+    }
+
+    await home.save()
 
   } catch (error) {
     console.log(error);
@@ -56,7 +69,7 @@ const postEditHome = async (req, res, next) => {
 const postDeleteHome = async (req, res, next) => {
   try {
     const homeId = req.params.homeId
-    await Home.deleteOne({_id: homeId})
+    await Home.deleteOne({ _id: homeId })
 
   } catch (err) {
     if (err) {
@@ -70,16 +83,16 @@ const postDeleteHome = async (req, res, next) => {
 
 const postAddHome = async (req, res, next) => {
   try {
-    const photoPath = req.file ? `/${req.file.path}` : "/uploads/default-home.jpg";    
+    const photoPath = req.file ? req.file.filename : "default-home.jpg";
     const { houseName, description, price, location, rating } = req.body
 
     const newHome = new Home({ houseName, description, price, location, rating, photo: photoPath })
     await newHome.save()
-    
-    return res.render("host/home-added", { 
-      pageTitle: "Home success page", 
-      isLoggedIn: req.isLoggedIn, 
-      user: req.user 
+
+    return res.render("host/home-added", {
+      pageTitle: "Home success page",
+      isLoggedIn: req.isLoggedIn,
+      user: req.user
     })
 
   } catch (error) {
@@ -90,11 +103,11 @@ const postAddHome = async (req, res, next) => {
 
 const getHostHomes = (req, res, next) => {
   Home.find().then((homes) => {
-    res.render("host/host-home-list", { 
-      pageTitle: "Host Homes List", 
-      homes, 
-      isLoggedIn: req.isLoggedIn, 
-      user: req.user 
+    res.render("host/host-home-list", {
+      pageTitle: "Host Homes List",
+      homes,
+      isLoggedIn: req.isLoggedIn,
+      user: req.user
     })
   })
 }
@@ -103,10 +116,10 @@ const getHostHomes = (req, res, next) => {
 
 const hostProtect = (req, res, next) => {
   const userType = req.user.role;
-  
-  if(userType === "host"){
-    next()  
-  }else{
+
+  if (userType === "host") {
+    next()
+  } else {
     return res.redirect("/")
   }
 }
